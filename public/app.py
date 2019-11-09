8b90961379a88b426fc1126c69c1e158d9f93ef5
Sangabanon <jacobrheingold@gmail.com>
2019-11-09 15:14:36 -0800

    Made changes

--- public/app.py
+++ public/app.py
@@ -1,4 +1,12 @@
-import os 
+from flask import Flask
 
+app = Flask(__name__)
 
-print(1)
+@app.route('/')
+def hello_world():
+    return "Hello World!!!!!"
+    
+
+if __name__ == '__main__':
+    app.run() # stops here
+    print('Server died')
